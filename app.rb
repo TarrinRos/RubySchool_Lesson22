bd7795b5990aca203572f68d7cd88d783b12287a
TarrinRos <tarindis@gmail.com>
2017-08-26 15:20:41 +0300

    Add post proc at contacts, and some addons for visit

--- app.rb
+++ app.rb
@@ -20,12 +20,35 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/after_visit' do
+  erb :after_visit
+end
+
+get '/after_send' do
+  erb :after_send
+end
+
 post '/visit' do
   @username = params[:username]
   @surname = params[:surname]
   @phone = params[:phone]
   @date_time = params[:date_time]
+  @barber_master = params[:barber_master]
+  @after_visit = "Спасибо #{@username}, что Вы к нам записались"
+
   f = File.open './public/users.txt', 'a'
-  f.write "Имя: #{@username}, Фамилия: #{@surname}, Номер телефона #{@phone},Время посещения: #{@date_time}"
+  f.write "Имя: #{@username}, Фамилия: #{@surname}, Номер телефона #{@phone},Время посещения: #{@date_time}, Мастер: #{@barber_master}"
   f.close
+  erb :after_visit
+end
+
+post '/contacts' do
+  @email = params[:email]
+  @message = params[:message]
+  @after_send = 'Спасибо Вам, за Ваше сообщение.'
+
+  f = File.open './public/contacts.txt', 'a'
+  f.write "Почта: #{@email}, Сообщения: #{@message}"
+  f.close
+  erb :after_send
 end
